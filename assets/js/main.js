e0d3e6b3c3a7c4faa6b0722ba45d2aac1f16e62f
Jake <88779170+jakeohanlon@users.noreply.github.com>
2023-08-18 13:19:47 +0800

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,9 @@
   /**
    * Easy selector helper function
    */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
+  const select = (selector) => {
+    return document.querySelector(selector);
+  };
 
   /**
    * Easy event listener function
@@ -27,62 +22,73 @@
     }
   }
 
-  /**
-   * Easy on scroll event listener 
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
+  
+/**
+ * Easy on scroll event listener
+ */
+const onscroll = (el, listener) => {
+  el.addEventListener('scroll', listener);
+};
 
-  /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
+/**
+ * Navbar links active state on scroll
+ */
+const navbarlinksActive = () => {
+  let header = select('#header');
+  let offset = header.offsetHeight + 10; // Adjust the offset here
+  let position = window.scrollY + offset;
 
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
+  navbarlinks.forEach(navbarlink => {
+    if (!navbarlink.hash) return;
+    let section = select(navbarlink.hash);
+    if (!section) return;
 
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
+    let sectionPos = section.getBoundingClientRect().top + window.scrollY - offset;
 
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
-   */
-  let selectHeader = select('#header')
-  if (selectHeader) {
-    const headerScrolled = () => {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled')
-      } else {
-        selectHeader.classList.remove('header-scrolled')
-      }
+    if (position >= sectionPos && position <= (sectionPos + section.offsetHeight)) {
+      navbarlink.classList.add('active');
+    } else {
+      navbarlink.classList.remove('active');
     }
-    window.addEventListener('load', headerScrolled)
-    onscroll(document, headerScrolled)
-  }
+  });
+};
+
+window.addEventListener('load', navbarlinksActive);
+onscroll(document, navbarlinksActive);
+
+/**
+ * Scrolls to an element with header offset
+ */
+const scrollto = (el) => {
+  let header = select('#header');
+  let offset = header.offsetHeight;
+  let element = select(el);
+  
+  if (!element) return;
+  
+  let elementPos = element.offsetTop - offset;
+  
+  window.scrollTo({
+    top: elementPos,
+    behavior: 'smooth'
+  });
+};
+
+/**
+ * Toggle .header-scrolled class to #header when page is scrolled
+ */
+let selectHeader = select('#header');
+if (selectHeader) {
+  const headerScrolled = () => {
+    if (window.scrollY > 100) {
+      selectHeader.classList.add('header-scrolled');
+    } else {
+      selectHeader.classList.remove('header-scrolled');
+    }
+  };
+  window.addEventListener('load', headerScrolled);
+  onscroll(document, headerScrolled);
+}
 
   /**
    * Back to top button
